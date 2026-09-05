54a074fc33d7b232c2d1f4534a25728f9889208b
Agil Mammadov <mammadovagil@proton.me>
2026-09-05 23:50:45 +0400

    feat(profile): rebrand to Bekaros 01

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
-iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_name="bekaros"
+iso_label="BEKAROS_01"
+iso_publisher="Bekars Space <https://bekars.space>"
+iso_application="Bekaros Linux Live/Rescue DVD"
+iso_version="01"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
